bbc02b27f14fd2d832f3836c641d2a060612dcbc
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-06-28 00:43:03 -0700

    signin/signup pages routing and rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import SignInForm from "./components/SignInForm";
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import { SignInForm, SignUpForm } from "./components";
 import Canvas from "./canvas";
+
 function App() {
   return (
-    <main className="app transition-all ease-in">
-      <SignInForm />
-      {/* <Canvas /> */}
-    </main>
+    <Routes>
+      <Route path="/signin" element={<SignInForm />} />
+      <Route path="/signup" element={<SignUpForm />} />
+      {/* <Route path="/canvas" element={<Canvas />} /> */}
+    </Routes>
   );
 }
 
--- client/src/components/SignInForm.jsx
+++ client/src/components/SignInForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+// import { Link, useHistory } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 
@@ -79,17 +80,17 @@ const SignInForm = () => {
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label
-                  htmlFor="email"
+                  htmlFor="username"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Email address
+                  Username address
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="username"
+                    name="username"
+                    type="username"
+                    autoComplete="username"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -138,7 +139,7 @@ const SignInForm = () => {
             <p className="mt-10 text-center text-sm text-gray-500">
               Don't have an account?{" "}
               <a
-                href="#"
+                href="/signup"
                 className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
               >
                 Sign up now
