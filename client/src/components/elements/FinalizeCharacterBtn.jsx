5660d39366e0f516cc69ee4a8a28340b06c55c27
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-06-29 11:12:45 -0700

    added more styling to finalize character button

--- client/src/components/elements/FinalizeCharacterBtn.jsx
+++ client/src/components/elements/FinalizeCharacterBtn.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import { fadeAnimation, slideAnimation } from "../../config/motion";
 
 const FinalizeCharacterBtn = () => {
+  const [hovered, setHovered] = useState(false);
   const navigate = useNavigate();
+
+  const handleMouseEnter = () => setHovered(true);
+  const handleMouseLeave = () => setHovered(false);
+
+  const buttonStyle = {
+    position: "fixed",
+    left: 600,
+    bottom: 0,
+    padding: "15px",
+    backgroundColor: `${
+      hovered ? "rgba(91, 189, 235, 1)" : "rgba(99, 191, 235, 0.75)"
+    }`,
+    transform: `translateY(-50%) ${hovered ? "scale(1.1)" : "scale(1)"}`,
+    color: "white",
+    fontSize: "1rem",
+    border: "none",
+    borderRadius: "7px",
+    width: "20%",
+    display: "flex",
+    justifyContent: "center",
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate("/home");
@@ -13,21 +35,10 @@ const FinalizeCharacterBtn = () => {
     <AnimatePresence>
       <motion.div {...slideAnimation("down")}>
         <button
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           type="submit"
-          style={{
-            position: "fixed",
-            left: 600,
-            bottom: 15,
-            padding: "15px",
-            backgroundColor: "#03a9f4",
-            color: "white",
-            fontSize: "1rem",
-            border: "none",
-            borderRadius: "7px",
-            width: "20%",
-            display: "flex",
-            justifyContent: "center",
-          }}
+          style={buttonStyle}
           onSubmit={() => {}}
           onClick={handleSubmit}
         >
